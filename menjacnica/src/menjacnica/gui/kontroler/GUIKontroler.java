fd37a3b90aaa372ee10514ea9d0fa82c2d533cfe
VanjaVlahovic <vlahovicvanja@gmail.com>
2018-04-22 12:37:41 +0200

    Commit sa klasom GUIKontroler

---
+++ menjacnica/src/menjacnica/gui/kontroler/GUIKontroler.java
@@ -0,0 +1,153 @@
+package menjacnica.gui.kontroler;
+
+import java.awt.EventQueue;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+import menjacnica.Menjacnica;
+import menjacnica.MenjacnicaInterface;
+import menjacnica.Valuta;
+import menjacnica.gui.DodajKursGUI;
+import menjacnica.gui.IzvrsiZamenuGUI;
+import menjacnica.gui.MenjacnicaGUI;
+import menjacnica.gui.ObrisiKursGUI;
+import menjacnica.gui.models.MenjacnicaTableModel;
+
+public class GUIKontroler {
+	
+	public static MenjacnicaInterface menjacnica = new Menjacnica();
+	public static MenjacnicaGUI gp;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIKontroler.gp = new MenjacnicaGUI();
+					GUIKontroler.gp.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public static void prikaziAboutProzor(){
+		JOptionPane.showMessageDialog(gp,
+				"Autor: Bojan Tomic, Verzija 1.0", "O programu Menjacnica",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	public static void sacuvajUFajl() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showSaveDialog(null);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+
+				menjacnica.sacuvajUFajl(file.getAbsolutePath());
+			}
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(gp, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	public static void ucitajIzFajla() {
+		try {
+			JFileChooser fc = new JFileChooser();
+			int returnVal = fc.showOpenDialog(null);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				menjacnica.ucitajIzFajla(file.getAbsolutePath());
+				gp.prikaziSveValute();
+			}	
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(gp, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	
+	
+	public static void prikaziDodajKursGUI() {
+		DodajKursGUI prozor = new DodajKursGUI();
+		prozor.setLocationRelativeTo(gp);
+		prozor.setVisible(true);
+	}
+
+	public static void prikaziObrisiKursGUI(JTable table) {
+		if (table.getSelectedRow() != -1) {
+			MenjacnicaTableModel model = (MenjacnicaTableModel)(table.getModel());
+			ObrisiKursGUI prozor = new ObrisiKursGUI(model.vratiValutu(table.getSelectedRow()));
+			prozor.setLocationRelativeTo(gp);
+			prozor.setVisible(true);
+		}
+	}
+	
+	public static void prikaziIzvrsiZamenuGUI(JTable table) {
+		if (table.getSelectedRow() != -1) {
+			MenjacnicaTableModel model = (MenjacnicaTableModel)(table.getModel());
+			IzvrsiZamenuGUI prozor = new IzvrsiZamenuGUI(model.vratiValutu(table.getSelectedRow()));
+			prozor.setLocationRelativeTo(gp);
+			prozor.setVisible(true);
+		}
+	}
+	
+	public static void unesiKurs(String naziv, String skraceniNaziv, int sifra, 
+			double prodajni, double kupovni, double srednji) {
+		try {
+			Valuta valuta = new Valuta();
+
+			// Punjenje podataka o valuti
+			valuta.setNaziv(naziv);
+			valuta.setSkraceniNaziv(skraceniNaziv);
+			valuta.setSifra(sifra);
+			valuta.setProdajni(prodajni);
+			valuta.setKupovni(kupovni);
+			valuta.setSrednji(srednji);
+			
+			// Dodavanje valute u kursnu listu
+			menjacnica.dodajValutu(valuta);
+
+			// Osvezavanje glavnog prozora
+			gp.prikaziSveValute();
+			
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(null, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public static void obrisiValutu(Valuta valuta) {
+		try{
+			menjacnica.obrisiValutu(valuta);
+			
+			gp.prikaziSveValute();
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(null, e1.getMessage(),
+					"Greska", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public static double izvrsiZamenu(Valuta valuta, boolean opcija, double iznos){
+		try{
+			double konacniIznos = 
+					menjacnica.izvrsiTransakciju(valuta,
+							opcija, iznos);
+		
+			return konacniIznos;
+		} catch (Exception e1) {
+		JOptionPane.showMessageDialog(gp, e1.getMessage(),
+				"Greska", JOptionPane.ERROR_MESSAGE);
+		return 0;
+	}
+	}
+}
